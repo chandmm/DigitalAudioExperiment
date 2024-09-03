3a586a40c7ddfea3998e22f475c6a833b82f28c9
ChandMM <michael.mchand@gmail.com>
2024-09-03 22:42:40 +1200

    Code update - work in progress

---
+++ DigitalAudioExperiment/ViewModel/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DigitalAudioExperiment.ViewModel
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged, IDisposable
+    {
+        protected bool _isDisposed;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        #region Events
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        #region Dispose
+
+        protected abstract void Dispose(bool disposing);
+
+        public virtual void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
--- DigitalAudioExperiment/ViewModel/DaeReceiverViewModel.cs
+++ DigitalAudioExperiment/ViewModel/DaeReceiverViewModel.cs
@@ -1,6 +1,22 @@
 ﻿namespace DigitalAudioExperiment.ViewModel
 {
-    public class DaeReceiverViewModel
+    public class DaeReceiverViewModel : BaseViewModel
     {
+
+        #region Dispose
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
     }
 }
