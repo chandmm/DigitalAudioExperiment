127cac4d154a54e2bccaafc330e5fd2576feef19
ChandMM <michael.mchand@gmail.com>
2024-09-15 21:27:37 +1200

    Adding playback logic - work in progress

---
+++ DigitalAudioExperiment/Logic/DaeAudioPlayer.cs
@@ -0,0 +1,79 @@
+﻿using Mp3DecoderSimple;
+using NAudio.Wave;
+
+namespace DigitalAudioExperiment.Logic
+{
+    public class DaeAudioPlayer : IDisposable
+    {
+        #region Fields
+
+        private bool _isDisposed;
+        private string _fileName;
+        private SimpleDecoder? _simpleDecoder;
+
+        #endregion
+
+        #region Initialisation
+
+        public DaeAudioPlayer(string fileName)
+        {
+            _fileName = fileName;
+
+            _simpleDecoder = new SimpleDecoder(fileName, null);
+        }
+
+        #endregion
+
+        #region Logic
+
+        public void Play()
+        {
+            if (_simpleDecoder == null)
+            {
+                return;
+            }
+
+            using (var simpleStream = _simpleDecoder.GetStream())
+            {
+
+            }
+        }
+
+        private void InternalPlay()
+        {
+            
+        }
+
+        #endregion
+
+        #region Fetch Playback Information Logic
+
+        public SimpleDecoder? GetDecoder()
+            => _simpleDecoder;
+
+        #endregion
+
+        #region Dispose
+
+        private void Dispose(bool _isDisposing)
+        {
+            if (!_isDisposed)
+            {
+                if (_isDisposing)
+                {
+                    _simpleDecoder?.Dispose();
+                    _simpleDecoder = null;
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
